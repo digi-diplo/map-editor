e42d0d48aea79d4d84af5742dcba7e6324cfc34d
MonsieurMan <ange.g.picard@gmail.com>
2018-08-07 15:56:33 +0200

    Fix tests

--- src/app/core/components/area/area.component.spec.ts
+++ src/app/core/components/area/area.component.spec.ts
@@ -3,6 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { AreaComponent } from './area.component';
 import { RegionComponent } from '../region/region.component';
 import { TerrainType, AreaType } from '../../state';
+import { MatSlideToggleModule } from '../../../../../node_modules/@angular/material';
 
 describe('AreaComponent', () => {
   let component: AreaComponent;
@@ -10,7 +11,8 @@ describe('AreaComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [AreaComponent, RegionComponent]
+      declarations: [AreaComponent, RegionComponent],
+      imports: [MatSlideToggleModule]
     })
       .compileComponents();
   }));
