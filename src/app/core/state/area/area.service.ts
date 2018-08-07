4924192f3de1fa21b38469cda6e061a4a12448bc
MonsieurMan <ange.g.picard@gmail.com>
2018-08-07 11:45:22 +0200

    Add point to closest segment index instead of simply pushing to array

--- src/app/core/state/area/area.service.ts
+++ src/app/core/state/area/area.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from '@angular/core';
-import { remove, update, ID } from '@datorama/akita';
+import { remove, update, ID, splice } from '@datorama/akita';
 
 
-import { Coords } from 'src/app/models';
+import { Coords, distanceBetween, distanceToSegment } from 'src/app/models';
 
 import { AreaStore } from './area.store';
 import { Area, createArea } from './area.model';
+
+export interface Line {
+  p1: Coords;
+  p2: Coords;
+}
 
 @Injectable({
   providedIn: 'root'
@@ -41,14 +46,41 @@ export class AreaService {
 
   addPointToActiveArea(point: Coords) {
     console.log('Adding point');
-    this.areasStore.updateActive(area => ({
-      ...area,
-      boundaries: [
-        ...area.boundaries,
-        point
-      ]
-    }));
+
+    this.areasStore.updateActive(area => {
+      const closestPointIndex = this.findInsertIndex(point, area.boundaries);
+      return {
+        ...area,
+        boundaries: [
+          ...splice(area.boundaries, closestPointIndex, 0, point)
+        ]
+      };
+    });
   }
+
+  /** Returns the index of the closest point. */
+  private findInsertIndex(point: Coords, points: Coords[]): number {
+    let insertIndex = 0;
+    let distanceToLine = Infinity;
+
+    for (let i = 0; i < points.length; i++) {
+      const p1 = points[i];
+      const secIndex = i + 1 < points.length ? i + 1 : 0;
+      const p2 = points[secIndex];
+      const actualDistance = distanceToSegment(point, { p1, p2 });
+
+      if (actualDistance < distanceToLine) {
+        distanceToLine = actualDistance;
+        insertIndex = secIndex;
+      }
+    }
+    return insertIndex;
+  }
+
+  // private distanceToLine(point: Coords, line: Line): number {
+  //   return Math.abs((line.p2.y - line.p1.y) * point.x - (line.p2.x - line.p1.x) * point.y + line.p2.x * line.p1.y - line.p2.y * line.p1.x)
+  //     / Math.sqrt(Math.pow(line.p2.y - line.p1.y, 2) + Math.pow(line.p2.x - line.p1.x, 2));
+  // }
 
   removePointToActiveRegion(index: number) {
     console.log('Removing point');
--- src/app/models/coords.ts
+++ src/app/models/coords.ts
@@ -3,9 +3,41 @@ export interface Coords {
   y: number;
 }
 
+export interface Line {
+  p1: Coords;
+  p2: Coords;
+}
+
 export function distanceBetween(a: Coords, b: Coords): number {
   return Math.sqrt(
     Math.pow(b.x - a.x, 2)
     + Math.pow(b.y - a.y, 2)
   );
 }
+
+export function distanceToSegment(p: Coords, segment: Line): number {
+  return Math.sqrt(distToSegmentSquared(p, segment.p1, segment.p2));
+}
+
+function square(x: number) {
+  return x * x;
+}
+
+function distanceSquared(v: Coords, w: Coords) {
+  return square(v.x - w.x) + square(v.y - w.y);
+}
+
+function distToSegmentSquared(p: Coords, v: Coords, w: Coords): number {
+  const l2 = distanceSquared(v, w);
+  if (l2 === 0) {
+    return distanceSquared(p, v);
+  }
+  let t = ((p.x - v.x) * (w.x - v.x) + (p.y - v.y) * (w.y - v.y)) / l2;
+  t = Math.max(0, Math.min(1, t));
+
+  return distanceSquared(p, {
+    x: v.x + t * (w.x - v.x),
+    y: v.y + t * (w.y - v.y)
+  });
+}
+
